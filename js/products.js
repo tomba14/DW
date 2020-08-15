4d4d4cb2dea1d0ba9ce4bf6980c83f479f00d2ef
tomba14 <55693976+tomba14@users.noreply.github.com>
2020-08-15 19:24:33 -0300

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var pArray = [];
+
+function mostrarprod(variablenashe) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < variablenashe.length; i++) {
+        let product = variablenashe[i];
+
+        htmlContentToAppend += `
+        <a href="product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">`+ product.name +`</h4>
+                    <small class="text-muted">` + product.productCount + ` artículos</small>
+                </div>
+                <p class="mb-1">` + product.description + `</p>
+            </div>
+        </div>
+    </a>
+        `
+
+        document.getElementById("plcontainer").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            pArray = resultObj.data;
+            mostrarprod(pArray);
+        }
+    });
 });
